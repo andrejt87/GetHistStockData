cc82d4dea016dd617fbdea79b434cf59912c47a1
Andrej <andrej.tupikin@googlemail.com>
2017-02-19 02:23:50 +0100

    DataMerge

--- GetHistStockData.py
+++ GetHistStockData.py
@@ -26,19 +26,40 @@ def ReadMnemonicCSV():
     MnemonicData = pd.read_csv('/Users/andrejtupikin/pythonfirststeps/GetXetraMnemonic/Data/Mnemonic.csv')
     
     for x in range(0,len (MnemonicData)):
-
+        
+        #GetDataURL
         StockDataURL = "https://www.google.com/finance/getprices?q=" + str(MnemonicData['Mnemonic'][x]) + "&x=" + Ex + "&i=" + Ticks_s + "&p=" + Dur + d_M_Y + "&f=" + Data
         
-        StockDataURL_Data = requests.get(StockDataURL,headers=headers).text
+        #GetActualData
+        strStockDataURL_Data = requests.get(StockDataURL,headers=headers).text
         
         # write to csv
-        csv_df = pd.DataFrame({str(MnemonicData['Mnemonic'][x]): StockDataURL_Data}, index=[x])
+        csv_df = pd.DataFrame({str(MnemonicData['Mnemonic'][x]): strStockDataURL_Data}, index=[x])
         csv_df.to_csv(Module_Data_Path + "DataGotOnDates/" + strftime("%d %b %Y", gmtime()) + "/" + Dur + d_M_Y + "_" + Ticks_s + "s/" + str(MnemonicData['Mnemonic'][x]) + '.csv', index=False)
-        
-        if not os.path.isfile(Module_Data_Path + "AllData/" + "/" + Dur + d_M_Y + "_" + Ticks_s + "s/" + str(MnemonicData['Mnemonic'][x]) + '.csv'):
-            csv_df.to_csv(Module_Data_Path + "AllData/" + "/" + Dur + d_M_Y + "_" + Ticks_s + "s/" + str(MnemonicData['Mnemonic'][x]) + '.csv', index=False)
+
+        #LoadExistingData
+        if os.path.isfile(Module_Data_Path + "AllData/" + "/" + Dur + d_M_Y + "_" + Ticks_s + "s/" + str(MnemonicData['Mnemonic'][x]) + '.csv'):
+    
+            ExistingData = pd.read_csv(Module_Data_Path + "AllData/" + "/" + Dur + d_M_Y + "_" + Ticks_s + "s/" + str(MnemonicData['Mnemonic'][x]) + '.csv')
+            strExistingData = str(ExistingData[str(MnemonicData['Mnemonic'][x])][0])
+            
+            arDatesOfExistingData = [m.start() for m in re.finditer('a14', strExistingData)]
+            
+            if not arDatesOfExistingData == []:
+                
+                 strLastDay = strExistingData[arDatesOfExistingData[-1]:arDatesOfExistingData[-1]+11]
+                 
+                 DateFound = strStockDataURL_Data.find(strLastDay)
+                 
+                 if not DateFound == -1:
+                     
+                     strMergedData = strExistingData[:arDatesOfExistingData[-1]] + strStockDataURL_Data[DateFound:]
+                     
+                     csv_df = pd.DataFrame({str(MnemonicData['Mnemonic'][x]): strMergedData}, index=[x])
+                     csv_df.to_csv(Module_Data_Path + "AllData/" + "/" + Dur + d_M_Y + "_" + Ticks_s + "s/" + str(MnemonicData['Mnemonic'][x]) + '.csv', index=False)
         
         print x, StockDataURL
+        
         
         
 def CreateFolder():
